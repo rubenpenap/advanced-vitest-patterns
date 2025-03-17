6a9a48b1f78e0c0f885b8e98c46924eb44928aed
Artem Zakharchenko <kettanaito@gmail.com>
2025-03-17 10:49:56 +0100

    clean the repo

--- exercises/01.setup/02.solution.multiple-workspaces/vite.config.ts
+++ exercises/01.setup/02.solution.multiple-workspaces/vite.config.ts
@@ -1,22 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
 	server: {
 		port: process.env.PORT ? Number(process.env.PORT) : undefined,
 	},
 	test: {
 		globals: true,
-		browser: {
-			enabled: true,
-			instances: [
-				{
-					browser: 'chromium',
-				},
-			],
-		},
 	},
 })
