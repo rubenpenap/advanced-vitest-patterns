4900ef90eb8cae5cd59e0f369ac6dc19fe454ce6
Artem Zakharchenko <kettanaito@gmail.com>
2025-04-27 12:09:19 +0200

    remove `vite`

--- exercises/04.performance/02.solution.concurrency/vitest.config.ts
+++ exercises/04.performance/02.solution.concurrency/vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-	server: {
-		port: process.env.PORT ? Number(process.env.PORT) : undefined,
-	},
 	test: {
 		globals: true,
 		maxConcurrency: 50,
