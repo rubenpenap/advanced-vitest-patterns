ea5e76b3db5c215a9378695dd79eed6a60965736
Artem Zakharchenko <kettanaito@gmail.com>
2025-05-08 14:26:36 +0200

    04/02: add `expect` scoping to the exercise

--- exercises/04.performance/02.problem.concurrency/src/one.test.ts
+++ exercises/04.performance/02.problem.concurrency/src/one.test.ts
@@ -3,7 +3,11 @@ import { setTimeout } from 'node:timers/promises'
 for (let i = 0; i < 100; i++) {
 	// 🐨 Replace the `test()` function below with `test.concurrent()`.
 	// This will make all the test cases in this test file run simultaneously.
-	test(`${i}`, async ({ expect }) => {
+	//
+	// 🐨 Spread the test context argument here and reference the `expect`
+	// function from that object.
+	// 💰 async ({ expect }) => {}
+	test(`${i}`, async () => {
 		await setTimeout(25)
 		expect(i).toBe(i)
 	})
