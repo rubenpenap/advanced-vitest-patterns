3e89d9f4442c006d97685b7b243e954acfddc97c
Artem Zakharchenko <kettanaito@gmail.com>
2025-06-16 14:26:44 +0200

    01/02: use `projects`, not `workspace`

--- exercises/01.setup/02.problem.multiple-workspaces/vitest.config.ts
+++ exercises/01.setup/02.problem.multiple-workspaces/vitest.config.ts
@@ -2,18 +2,18 @@ import { defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		// 🐨 Add a property called `workspace`.
+		// 🐨 Add a property called `projects`.
 		// Assign it an array as a value.
-		// 💰 workspace: []
+		// 💰 projects: []
 		//
-		// 🐨 Add the first object to the `workspace` array.
+		// 🐨 Add the first object to the `projects` array.
 		// Inside that object, define a property called `test`.
 		// This will be your configuration for unit tests.
 		// Describe it accordingly, giving it a distinct `name`,
 		// `environment`, and `include` and `exclude` patterns.
 		// 💰 test: { name: 'unit', environment: 'node', globals: true }
 		//
-		// 🐨 Add the second object to the `workspace` array.
+		// 🐨 Add the second object to the `projects` array.
 		// Similarly to the first one, this will be your configuration for edge tests.
 		// Give it a `name`, and use `edge-runtime` as its `environment`.
 		// Don't forget to specify the `include` pattern so it applies only
