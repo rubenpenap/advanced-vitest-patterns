54c56d82b1ffe0027f7f14134d43bf2202e8f293
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-09-23 19:05:54 -0300

    fix: update vitest configuration to include vitestProfiler plugin

--- playground/vitest.config.ts
+++ playground/vitest.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vitest/config'
-// 🐨 Import `vitestProfiler` from `vitest-profiler/plugin`
+import { vitestProfiler } from 'vitest-profiler/plugin'
 
 export default defineConfig({
-	// 🐨 Add a `plugins` property at the root of the Vitest configuration.
-	// Make it equal an array. Inside that array, call the `vitestProfiler`
-	// function you imported earlier.
-	// 💰 plugins: [somePlugin()],
+	plugins: [vitestProfiler()],
 	test: {
 		globals: true,
 	},
