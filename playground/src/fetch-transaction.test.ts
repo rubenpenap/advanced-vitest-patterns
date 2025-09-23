b6730858f8cc98fef436b8a16e3b4998f1e8d567
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-09-23 17:42:06 -0300

    test: add assertions for transaction fetching to validate schema and properties

--- playground/src/fetch-transaction.test.ts
+++ playground/src/fetch-transaction.test.ts
@@ -4,10 +4,9 @@ import { userSchema } from './schemas'
 test('fetches a transaction between two users', async () => {
 	const transaction = await fetchTransaction('transaction-1')
 
-	// 🐨 Write an assertion over the `transaction` object.
-	// In the expected object, express that the `id` property should
-	// equal to the transaction ID, and both `issuer` and `recipient`
-	// properties should match the `userSchema` schema.
-	// 💰 expect(transaction).toEqual<Transaction>({})
-	// 💰 expect.toMatchSchema(schema)
+	expect(transaction).toEqual<Transaction>({
+		id: 'transaction-1',
+		issuer: expect.toMatchSchema(userSchema),
+		recipient: expect.toMatchSchema(userSchema),
+	})
 })
