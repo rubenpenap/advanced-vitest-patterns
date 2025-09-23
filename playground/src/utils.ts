de737a4a468be97c80de0cc019c62271b190f434
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-09-23 20:25:37 -0300

    refactor: rename exercise to concurrency, update README, and adjust Vitest configuration for improved test performance

--- playground/src/utils.ts
+++
@@ -1,9 +0,0 @@
-export function expensiveCompute() {
-	const rows = []
-
-	for (let i = 0; i < 10_000_000; i++) {
-		rows.push(i)
-	}
-
-	return rows
-}
--- playground/vitest.config.ts
+++ playground/vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config'
-import { vitestProfiler } from 'vitest-profiler/plugin'
 
 export default defineConfig({
-	plugins: [vitestProfiler()],
 	test: {
 		globals: true,
+		maxConcurrency: 50,
 	},
 })
