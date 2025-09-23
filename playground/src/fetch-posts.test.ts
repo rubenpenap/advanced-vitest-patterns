7f3d6767a7eacabc2c876eb1c9707073276adce3
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-09-23 18:39:44 -0300

    test: use soft assertions for response validation in fetchPosts test

--- playground/src/fetch-posts.test.ts
+++ playground/src/fetch-posts.test.ts
@@ -3,11 +3,11 @@ import { fetchPosts } from './fetch-posts'
 test('returns a list of posts', async () => {
 	const response = await fetchPosts()
 
-	expect(response.status).toBe(200)
-	expect(response.headers.get('cache-control')).toBe(
-		'public, max-age=31536000, immutable',
-	)
-	await expect(response.json()).resolves.toEqual([
+	expect.soft(response.status).toBe(200)
+	expect
+		.soft(response.headers.get('cache-control'))
+		.toBe('public, max-age=31536000, immutable')
+	await expect.soft(response.json()).resolves.toEqual([
 		{
 			id: expect.any(String),
 			title: 'Soft Assertions',
