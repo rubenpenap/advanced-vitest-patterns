649935a63745a0d5ef5a284fb5b2326e902dc4d4
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-09-23 16:56:54 -0300

    test: simplify user schema validation in fetchUser test by using custom matcher

--- playground/src/fetch-user.test.ts
+++ playground/src/fetch-user.test.ts
@@ -4,19 +4,5 @@ import { userSchema } from './schemas'
 test('returns the user by id', async () => {
 	const user = await fetchUser('abc-123')
 
-	// 💣 Remove this object parsing.
-	// Your custom matcher will perform the parsing now.
-	const result = userSchema.safeParse(user)
-
-	// 💣 Remove both of these assertions.
-	expect(result.error).toBeUndefined()
-	expect(result.data).toEqual({
-		id: 'abc-123',
-		name: 'John Maverick',
-	})
-
-	// 🐨 Add a single assertion over the `user` object
-	// as the received value, using the `.toMatchSchema()` matcher.
-	// Provide the `userSchema` as the expected value.
-	// 💰 expect(this).toMatchSchema(that)
+	expect(user).toMatchSchema(userSchema)
 })
