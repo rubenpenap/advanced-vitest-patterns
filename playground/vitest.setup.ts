1be60864181b3039a5caa1348da3334e68e0e2ab
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-09-23 18:08:42 -0300

    refactor: implement custom equality tester for Measurement instances in Vitest

--- playground/vitest.setup.ts
+++ playground/vitest.setup.ts
@@ -1,17 +1,9 @@
 import { Measurement } from './src/measurement'
 
-// 🐨 Extend default equality testers in Vitest by calling
-// `expect.addEqualityTesters` function and passing it an array as an argument.
-// 💰 expect.addEqualityTesters([])
-
-// 🐨 In the list of custom equality testers, declare a new function
-// called `measurementTester`. It accepts two parameters: `received` and `expected`.
-// 💰 function measurementTester(received, expected) {}
-
-// 🐨 Then, check that both the `received` and `expected` values are instances
-// of the `Measurement` class.
-// 💰 if (one instanceof Measurement && another instanceof Measurement) {}
-
-// 🐨 Next, if both values are measurements, compare them using the `.equals()`
-// method of the `Measurement` class. Return the result of that comparison.
-// 💰 return expected.equals(received)
+expect.addEqualityTesters([
+	function measurementTester(received, expected) {
+		if (received instanceof Measurement && expected instanceof Measurement) {
+			return expected.equals(received)
+		}
+	},
+])
