306535bf663311cf2a14bca88de5ff7fd5ad9ba7
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-09-23 20:28:23 -0300

    refactor: update test cases to use test.concurrent for improved execution efficiency

--- playground/src/two.test.ts
+++ playground/src/two.test.ts
@@ -1,13 +1,7 @@
 import { setTimeout } from 'node:timers/promises'
 
 for (let i = 0; i < 100; i++) {
-	// 🐨 Replace the `test()` function below with `test.concurrent()`.
-	// This will make all the test cases in this test file run simultaneously.
-	//
-	// 🐨 Spread the test context argument here and reference the `expect`
-	// function from that object.
-	// 💰 async ({ expect }) => {}
-	test(`${i}`, async () => {
+	test.concurrent(`${i}`, async ({ expect }) => {
 		await setTimeout(25)
 		expect(i).toBe(i)
 	})
