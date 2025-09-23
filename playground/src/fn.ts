560941ee559b8f51cec9eff5abf97b6b88cbbb62
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-09-23 16:24:27 -0300

    refactor: update playground setup to use custom fixtures and remove unused dependencies

--- playground/src/fn.ts
+++
@@ -1,7 +0,0 @@
-export function fn(input: number) {
-	if (input > 10) {
-		return 'great'
-	}
-
-	return 'okay'
-}
--- playground/vitest.config.ts
+++ playground/vitest.config.ts
@@ -3,11 +3,5 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	test: {
 		globals: true,
-		coverage: {
-			enabled: true,
-			include: ['src/**/*.ts'],
-			provider: 'v8',
-			reporter: ['text', 'html'],
-		},
 	},
 })
